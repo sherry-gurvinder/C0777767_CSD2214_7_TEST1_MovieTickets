5f083a85a9cd08e8db0c6967e43244e6189fcb63
sherry-gurvinder <sgurvinder0594@gmail.com>
2021-03-11 18:22:38 -0500

    4th commit

--- javascript.js
+++ javascript.js
@@ -3,32 +3,41 @@ $( document ).ready(function() {
 
 var movieseat=0;
 var moviename;
+var price=0;
+
 $("#movie").change(function()
 {
   
    moviename = $("#movie").val();
    if(moviename == "John Wick ($13.00)")
    {
-
+      document.getElementById("price").innerHTML="13.00";
+      document.getElementById("totalprice").innerHTML="13.00";
+      price=13;
    }
    if(moviename == "Gone Girl ($15.00)")
    {
-
+      document.getElementById("price").innerHTML="15.00";
+      document.getElementById("totalprice").innerHTML="15.00";
+      price=15;
    }
    if(moviename == "Arrival ($10.00)")
    {
-
+      document.getElementById("price").innerHTML="10.00";
+      document.getElementById("totalprice").innerHTML="10.00";
+      price=10;
    }
    
-   alert(moviename);
+  
 })
 
  $(".movieseat").click(function()
  {
-  
+   document.getElementById("totalprice").innerHTML="";
    $( this ).toggleClass("selected");
    movieseat++;
-   alert(movieseat);
+   document.getElementById("totalprice").innerHTML=movieseat*price;
+   
   
 
  })
