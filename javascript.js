f1f68269003cce6f56abf6d941789a3ab885750a
sherry-gurvinder <sgurvinder0594@gmail.com>
2021-03-11 18:16:01 -0500

    3rd commit

--- javascript.js
+++ javascript.js
@@ -1,13 +1,36 @@
 $( document ).ready(function() {
 
 
+var movieseat=0;
+var moviename;
+$("#movie").change(function()
+{
+  
+   moviename = $("#movie").val();
+   if(moviename == "John Wick ($13.00)")
+   {
+
+   }
+   if(moviename == "Gone Girl ($15.00)")
+   {
+
+   }
+   if(moviename == "Arrival ($10.00)")
+   {
+
+   }
+   
+   alert(moviename);
+})
 
  $(".movieseat").click(function()
  {
-   
+  
    $( this ).toggleClass("selected");
+   movieseat++;
+   alert(movieseat);
+  
 
-   
  })
 
 
